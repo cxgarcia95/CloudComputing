47a998823be3e42f835bd5a0f9b9c2acac0d36c7
Carlos Garcia <carlos.garcia@inovex.de>
2021-05-28 12:29:35 +0200

    functioning exercise 3

--- Exercise3/product-descp-service/product_descp.js
+++ Exercise3/product-descp-service/product_descp.js
@@ -8,5 +8,27 @@ module.exports = function (options) {
 
 
     //To DO: add the pattern functions and describe the logic inside the function
+    function productURL(msg, response)
+    {
+      for (i=0; i<mockData.length; i++)
+      {
+        if (mockData[i].product_id == msg.productId)
+        {
+          response(null, {result: mockData[i].product_url});
+        }
+      }
+      response(null, {result: ''});
+    }
 
+    function productName(msg, response)
+    {
+      for (i=0; i<mockData.length; i++)
+      {
+        if (mockData[i].product_id == msg.productId)
+        {
+          response(null, {result: mockData[i].product_name});
+        }
+      }
+      response(null, {result: ''});
+    }
 }
--- Exercise3/product-price-service/product_price.js
+++ Exercise3/product-price-service/product_price.js
@@ -2,8 +2,18 @@ module.exports = function (options) {
     //Import the mock data json file
     const mockData = require('./MOCK_DATA.json');
     //To DO: Add the patterns and their corresponding functions
-
+    this.add('role:Product,cmd:getProductPrice', productPrice)
 
     //To DO: add the pattern functions and describe the logic inside the function
-
+    function productPrice(msg, response)
+    {
+      for (i=0; i<mockData.length; i++)
+      {
+        if (mockData[i].product_id == msg.productId)
+        {
+          response(null, {result: mockData[i].product_price});
+        }
+      }
+      response(null, {result: 0.0});
+    }
 }
